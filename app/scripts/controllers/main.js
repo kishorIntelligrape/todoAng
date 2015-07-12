45510a6e71b71793f31012832ba3f9b48a671edb
Brij Kishor <brij.kishor@tothenew.com>
2015-07-12 13:05:30 +0530

    Refactor code

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,35 +9,36 @@
  */
 angular.module('mytodoApp')
   .controller('MainCtrl', ['$scope', 'todoService', function ($scope, todoService) {
-    	var todosInStore = localStorageService.get('todos');
+    //var todosInStore = localStorageService.get('todos');
+    console.log('MainCtrl loaded');
+    $scope.todos = [];
 
-		$scope.todos = todosInStore || [];
+    /*$scope.$watch('todos', function () {
+      localStorageService.set('todos', $scope.todos);
+    }, true);*/
+    $scope.todos = [];
 
-		$scope.$watch('todos', function () {
-		  localStorageService.set('todos', $scope.todos);
-		}, true);
-        $scope.todos = [];
+    $scope.getTodos = function () {
+      todoService.getTodos(function (data) {
+        console.log(data);
+      }, function (data, status) {
+        console.log(data);
+      });
+    };
 
-        $scope.getTodos = function () {
-            todoService.getTodos(function(data) {
-                console.log(data);   
-            }, function (data, status) {
-                console.log(data);                  
-            });
-        }
-		
-    	$scope.addTodo = function () {
-    		if ($scope.todo) {
-    			$scope.todos.push($scope.todo);
-    		}
-    		$scope.todo = '';
-    	};
+    $scope.addTodo = function () {
+      if ($scope.todo) {
+        $scope.todos.push($scope.todo);
+      }
+      $scope.todo = '';
+    };
 
-    	$scope.removeTodo = function (index) {
-		  $scope.todos.splice(index, 1);
-		};
-        $scope.init = function () {
-            $scope.getTodos();
-        };
-        $scope.init();
+    $scope.removeTodo = function (index) {
+      $scope.todos.splice(index, 1);
+    };
+    $scope.init = function () {
+      console.log("Main ctrl init is been called");
+      $scope.getTodos();
+    };
+    $scope.init();
   }]);
--- app/scripts/services/todoServices.js
+++ app/scripts/services/todoServices.js
@@ -1,41 +1,43 @@
 'use strict';
 
 angular.module('mytodoApp')
-.service('todoService', '$rootScope', ['$http', function ($http, $rootScope){
+  .service('todoService',  ['$http', '$rootScope', function ($http, $rootScope) {
 
-	var todoService = {
-		getTodos: function (successCallback, failCallback) {
-			$http({
-                        url: '',
-                        method: 'POST',
-                        headers: {'Content-Type': 'application/x-www-form-urlencoded',
-                                    'Authorization' : $rootScope.user.authorizationHeader
-                                  }
-                    }
-                ).success(function (data) {
-                        console.log('appointmentService:: data', data);
-                        successCallback(data);
-                    }).error(function (data, status) {
-                        failCallback(data, status);
-                    });
-		},
-        save: function (todo, successCallback, failCallback) {
-            $http({
-                    url: '',
-                    method: 'POST',
-                    headers: {'Content-Type': 'application/x-www-form-urlencoded',
-                                'Authorization' : $rootScope.user.authorizationHeader
-                              }
-                              
-                }
-            ).success(function (data) {
-                console.log('appointmentService:: data', data);
-                successCallback(data);
-            }).error(function (data, status) {
-                failCallback(data, status);
-            });
-        }
-	};
+    var todoService = {
+      getTodos: function (successCallback, failCallback) {
+        $http({
+            url: 'http://localhost:8080/todo-rest-api/api/todos',
+            method: 'GET',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': $rootScope.user.authorizationHeader
+            }
+          }
+        ).success(function (data) {
+            console.log('appointmentService:: data', data);
+            successCallback(data);
+          }).error(function (data, status) {
+            failCallback(data, status);
+          });
+      },
+      save: function (todo, successCallback, failCallback) {
+        $http({
+            url: 'http://localhost:8080/todo-rest-api/api/todos',
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/x-www-form-urlencoded',
+              'Authorization': $rootScope.user.authorizationHeader
+            }
 
-	return todoService;
-}]);
+          }
+        ).success(function (data) {
+            console.log('appointmentService:: data', data);
+            successCallback(data);
+          }).error(function (data, status) {
+            failCallback(data, status);
+          });
+      }
+    };
+
+    return todoService;
+  }]);
